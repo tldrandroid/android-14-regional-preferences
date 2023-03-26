70ecce32256929b91cf6f7320afabf4f52b435e1
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2023-03-26 19:15:10 -0400

    Add dynamic behavior to view model

--- app/src/main/java/com/tldrandroid/regionalpreferences/model/MainViewModel.kt
+++ app/src/main/java/com/tldrandroid/regionalpreferences/model/MainViewModel.kt
@@ -1,11 +1,15 @@
 package com.tldrandroid.regionalpreferences.model
 
+import androidx.core.text.util.LocalePreferences
+import androidx.core.text.util.LocalePreferences.HourCycle
+import androidx.core.text.util.LocalePreferences.TemperatureUnit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import java.util.Locale
 
 class MainViewModel : ViewModel() {
 
@@ -22,11 +26,33 @@ class MainViewModel : ViewModel() {
     }
 
     fun refreshLocalePreferences() {
+        val firstDayOfWeekSelection = LocalePreferences.getFirstDayOfWeek()
+            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
+
+        val firstDayOfWeek = firstDayOfWeekSelection.ifBlank {
+            "Sun"
+        }
+
+        val temperature = when (LocalePreferences.getTemperatureUnit()) {
+            TemperatureUnit.CELSIUS -> "21 C"
+            TemperatureUnit.FAHRENHEIT -> "78 F"
+            TemperatureUnit.KELVIN -> "299 K"
+            else -> "78 F"
+        }
+
+        val time = when (LocalePreferences.getHourCycle()) {
+            HourCycle.H11 -> "04:00pm"
+            HourCycle.H12 -> "04:00pm"
+            HourCycle.H23 -> "16:00"
+            HourCycle.H24 -> "16:00"
+            else -> "04:00pm"
+        }
+
         viewModelState.update {
             it.copy(
-                firstDayOfWeek = "Sun",
-                temperature = "78 F",
-                time = "04:00pm"
+                firstDayOfWeek = firstDayOfWeek,
+                temperature = temperature,
+                time = time
             )
         }
     }
